0a0b67536efccf95a401842c5d1879feca233f33
Oman <a336496@alumnos.uaslp.mx>
2025-09-12 10:49:28 -0600

    Agregados sprites

--- Practicas/src/entidad/Entidad.java
+++ Practicas/src/entidad/Entidad.java
@@ -1,8 +1,10 @@
 package entidad;
 
+import java.awt.image.BufferedImage;
+
 public class Entidad {
     protected int x, y, velocidad;
-    //protected BufferedImage arriba1, arriba2, abajo1, abajo2, derecha1, derecha2, izquierda1, izquieda2;
-    //protected String direccion;
+    protected BufferedImage arriba1, arriba2, abajo1, abajo2, derecha1, derecha2, izquierda1, izquieda2;
+    protected String direccion;
     
 }
